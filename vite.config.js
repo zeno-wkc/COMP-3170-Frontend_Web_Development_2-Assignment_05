1e325947e4154d45947298b31cb68d35dd142549
Zeno <zenowkc@gmail.com>
2024-10-26 19:32:08 -0700

    Create .gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/COMP-3170-Frontend_Web_Development_2-Assignment_04/'
+  base: '/COMP-3170-Frontend_Web_Development_2-Assignment_05/'
 })
